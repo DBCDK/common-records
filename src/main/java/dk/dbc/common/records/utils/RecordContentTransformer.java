d198766f3aa83fb707a4477ba861d2579ff68cf5
Anders Thrane Michelsen <atm@dbc.dk>
2017-08-23 11:48:39 +0000

    IS-2297 Flyt funktionaliteten i ExpandCommonRecord til rawrepo-access
    git-svn-id: https://svn.dbc.dk/repos/common-records/trunk@115398 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ src/main/java/dk/dbc/common/records/utils/RecordContentTransformer.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+ *  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+ */
+
+package dk.dbc.common.records.utils;
+
+import dk.dbc.common.records.MarcConverter;
+import dk.dbc.common.records.MarcRecord;
+import dk.dbc.common.records.MarcXchangeFactory;
+import dk.dbc.common.records.marcxchange.CollectionType;
+import dk.dbc.common.records.marcxchange.ObjectFactory;
+import dk.dbc.common.records.marcxchange.RecordType;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+
+public class RecordContentTransformer {
+    private static final XLogger logger = XLoggerFactory.getXLogger(RecordContentTransformer.class);
+    private static final String SCHEMA_LOCATION = "http://www.loc.gov/standards/iso25577/marcxchange-1-1.xsd";
+    private static final String ENCODING = "UTF-8";
+
+    /**
+     * Encodes the record as marcxchange.
+     *
+     * @param record The record to encode.
+     * @return The encoded record as a sequence of bytes.
+     * @throws JAXBException                if the record can not be encoded in marcxchange.
+     * @throws UnsupportedEncodingException if the record can not be encoded in UTF-8
+     */
+    public static byte[] encodeRecord(MarcRecord record) throws JAXBException, UnsupportedEncodingException {
+        logger.entry(record);
+        byte[] result = null;
+
+        try {
+            if (record.getFields().isEmpty()) {
+                return null;
+            }
+
+            RecordType marcXchangeType = MarcXchangeFactory.createMarcXchangeFromMarc(record);
+
+            ObjectFactory objectFactory = new ObjectFactory();
+            JAXBElement<RecordType> jAXBElement = objectFactory.createRecord(marcXchangeType);
+
+            JAXBContext jc = JAXBContext.newInstance(CollectionType.class);
+            Marshaller marshaller = jc.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, SCHEMA_LOCATION);
+
+            StringWriter recData = new StringWriter();
+            marshaller.marshal(jAXBElement, recData);
+
+            logger.info("Marshalled record: {}", recData.toString());
+            result = recData.toString().getBytes(ENCODING);
+
+            return result;
+        } finally {
+            logger.exit(result);
+        }
+    }
+
+    public static MarcRecord decodeRecord(byte[] bytes) throws UnsupportedEncodingException {
+        return MarcConverter.convertFromMarcXChange(new String(bytes, ENCODING));
+    }
+
+}
